6d43f3109ecdb96ca4e4b19b60f534a51ccf0bcc
shusaku009 <shusaku009@gmail.com>
2023-02-10 18:22:10 +0900

    add childメソッドを追加
    Userクラスに新しいメソッドChild?を追加した

--- lib/age.rb
+++ lib/age.rb
@@ -5,10 +5,13 @@ class User
   end
   
   def greet
-    if @age <= 12
+    if child?
       "ぼくは#{@name}だよ。"
     else
       "僕は#{@name}です。"
     end
+  end
+  def child?
+    @age <= 12
   end
 end
--- spec/age_spec.rb
+++ spec/age_spec.rb
@@ -1,32 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe User do
-  describe '#greet' do
-    let(:user) { User.new(name: 'たろう', age: age) }
-    subject { user.greet }
+  let(:user) { User.new(name: 'たろう', age: age) }
+  shared_context '12歳の場合' do
+    let(:age) { 12 }
+  end
+  shared_context '13歳の場合' do
+    let(:age) { 13 }
+  end
 
-    shared_examples '子どものあいさつ' do
+  describe '#greet' do
+    subject { user.greet }
+    context '12歳以下の場合' do
+      include_context '12歳の場合'  
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    context '0歳以下の場合' do
-      let(:age) { 0 }
-      it_behaves_like '子どものあいさつ'
-    end
-    context '12歳以下の場合' do
-      let(:age) { 12 }
-      it_behaves_like '子どものあいさつ'
-    end
-
-    shared_examples '大人のあいさつ' do
+    context '13歳以上の場合' do
+      include_context '13歳の場合'
       it { is_expected.to eq '僕はたろうです。' }
     end
-    context '13歳以上の場合' do
-      let(:age) { 13 }
-      it_behaves_like '大人のあいさつ'
+  end
+
+  describe '#child?' do
+    let(:user) { User.new(name: 'たろう', age: age) }
+    subject { user.child? }
+    context '12歳以下の場合' do
+      include_context '12歳の場合'
+      it { is_expected.to eq true }
     end
-    context '100歳以上の場合' do
-      let(:age) { 100 }
-      it_behaves_like '大人のあいさつ'
+    context '13歳以上の場合' do
+      include_context '13歳の場合'
+      it { is_expected.to eq false }
     end
   end
 end
