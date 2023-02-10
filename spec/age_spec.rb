65a5b0cd90b22542e042166789d90a8ebafa6d75
shusaku009 <shusaku009@gmail.com>
2023-02-10 17:49:00 +0900

    update exampleの再利用
    shared_examplesとit_behaves_likeの機能を追加

--- spec/age_spec.rb
+++ spec/age_spec.rb
@@ -4,16 +4,29 @@ RSpec.describe User do
   describe '#greet' do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
-    
+
+    shared_examples '子どものあいさつ' do
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
+    end
+    context '0歳以下の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子どものあいさつ'
+    end
     context '12歳以下の場合' do
       let(:age) { 12 }
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
+      it_behaves_like '子どものあいさつ'
+    end
+
+    shared_examples '大人のあいさつ' do
+      it { is_expected.to eq '僕はたろうです。' }
     end
     context '13歳以上の場合' do
       let(:age) { 13 }
-      it '漢字で答えること' do
-        is_expected.to eq '僕はたろうです。'
-      end
+      it_behaves_like '大人のあいさつ'
+    end
+    context '100歳以上の場合' do
+      let(:age) { 100 }
+      it_behaves_like '大人のあいさつ'
     end
   end
 end
