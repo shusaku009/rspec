9761985197e0eb859645e29cc514e35f398a85ef
shusaku009 <shusaku009@gmail.com>
2023-02-10 14:10:38 +0900

    Initial Commit

---
+++ spec/age_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+RSpec.describe User do
+  describe '#greet' do
+    let(:user) { User.new(**params) }
+    let(:params) { { name: 'たろう' , age: age } } 
+    subject { user.greet }
+    
+    context '12歳以下の場合' do
+      let(:age) { 12 }
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
+    end
+    context '13歳以上の場合' do
+      let(:age) { 13 }
+      it '漢字で答えること' do
+        is_expected.to eq '僕はたろうです。'
+      end
+    end
+  end
+end
